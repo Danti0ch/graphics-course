d932167428c540b5ba7ebaf04a3b157bdc68e134
Ivanov Mikhail <ivanov.mikhail@phystech.edu>
2025-03-04 18:16:14 +0300

    Fix numFramesInFlight

--- tasks/inflight_frames/App.cpp
+++ tasks/inflight_frames/App.cpp
@@ -40,7 +40,7 @@ App::App()
       .deviceExtensions = deviceExtensions,
       // Replace with an index if etna detects your preferred GPU incorrectly
       .physicalDeviceIndexOverride = {},
-      .numFramesInFlight = 1,
+      .numFramesInFlight = 2,
     });
   }
 
